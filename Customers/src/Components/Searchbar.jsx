2daf87866dfa723dbe3f5e84a62954670f82514e
Sayan Dey Sarkar <sayandeysarkar2003@gmail.com>
2023-02-14 00:59:08 +0530

    SearchBar

---
+++ Customers/src/Components/Searchbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+
+const Searchbar = styled.input`
+  width: 100%;
+  height: 3.5vw;
+
+  text-align: left;
+  padding-left: 15vw;
+
+  font-family: 'Inter';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    color: black;
+`
+
+export const Searchbar1 = () => {
+  return <Searchbar placeholder="Search for shops nearby" />;
+};
+
+export const Searchbar2 = () => {
+  return <Searchbar placeholder="Search in this shop" />;
+};
+
+export const Searchbar3 = () => {
+  return <Searchbar placeholder="Search for a product" />;
+};
+
+// export const Searchbar4 = (props) => {
+//   return <Searchbar placeholder="Search for {props} "/>;
+// };
