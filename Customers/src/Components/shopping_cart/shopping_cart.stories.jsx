2771602558f136a604a46c2befdd101d72aa81b4
Tushar Banik <107763774+Tushar98644@users.noreply.github.com>
2023-02-13 20:19:24 +0530

    Components and stories

---
+++ Customers/src/Components/shopping_cart/shopping_cart.stories.jsx
@@ -0,0 +1,11 @@
+import Shopping_cart from "./shopping_cart";
+
+export default {
+    title: 'Shopping_cart',
+    component: Shopping_cart,
+    parameters: {
+        layout: 'centered',
+    },
+}
+
+export const Shopping = () => <Shopping_cart />
