1ed9398965e20d68fc3ab0bdf1a5f4a7d1bb4566
Sayan Dey Sarkar <sayandeysarkar2003@gmail.com>
2023-02-15 19:02:52 +0530

    More Additions

---
+++ Shops/src/Components/ShopDetails1.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    background: #fff;
+    display: flex;
+    flex-direction: column;
+    gap: 2vw;
+`
+
+const Header = styled.div`
+    font-family: 'Inter', sans-serif;
+    font-size: 36px;
+    font-weight: 500;
+    text-align: left;
+`
+
+const SubHeader = styled.div`
+    font-family: 'Inter', sans-serif;
+    font-size: 24px;
+    font-weight: 500;
+    text-align: left;
+`
+
+const Text = styled.div`
+    font-family: 'Inter', sans-serif;
+    font-size: 16px;
+    font-weight: 400;
+    text-align: left;
+    color: #94A3B8;
+`
+
+const Input = styled.input`
+    background: #F7F9FC;
+    border: 1px solid #e4e2f038;
+    box-sizing: border-box;
+
+`
+
+const ShopDetails1 = () => {
+  return (
+    <Container>
+        <Header>Add Shop Details</Header>
+        <SubHeader>Shop Name</SubHeader>
+        <Input type="text" placeholder="Enter Shop Name" />
+        <SubHeader>Category</SubHeader>
+        <Input type="text" placeholder="Enter Category" />
+        <SubHeader>Shop Contact Number</SubHeader>
+        <Input type="text" placeholder="Enter Shop Contact Number" />
+        <SubHeader>Shop Address</SubHeader>
+        <Input type="text" placeholder="Enter Shop Address" />
+        <SubHeader>Landmark</SubHeader>
+        <Input type="text" placeholder="Enter nearest Landmark" />
+    </Container>
+  )
+}
+
+export default ShopDetails1
--- Shops/src/Components/VendorProfile.jsx
+++ Shops/src/Components/VendorProfile.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
+import VendorPic from "../Assets/VendorPic";
 
+const Box = styled.div`
+    background: #fff;
+    border-radius: 8px;
 
-const VendorProfile = () => {
+    width: 22.4vw;
+    height: 33.5vw;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+
+    font-family: 'Inter', sans-serif;
+
+`
+
+const VendorProfile = (props) => {
   return (
-    <div>VendorProfile</div>
+    <Box>
+        {VendorPic}
+        <h1>{props.name?props.name:"Vendor Name"}</h1>
+        <br></br>
+        <h2>PH No.--- {props.phone?props.phone:"1234567890"}</h2>
+        <h2>EMAIL--- {props.email?props.email:"xxxxxxxxxxxx@gmail.com"}</h2>
+    </Box>
   )
 }
 
