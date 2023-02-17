aeb6d6816106d1eb3b28cdeefa0327e57ca805fb
Sayan Dey Sarkar <sayandeysarkar2003@gmail.com>
2023-02-17 22:01:07 +0530

    changes

--- user/src/Pages/Desktop8.jsx
+++ user/src/Pages/Desktop8.jsx
@@ -1,55 +1,102 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 import ShopDescription from "../Components/ShopDescription";
 import ItemCard from "../Components/ItemCard2";
-import Navbar1 from '../Components/Navbar/Navbar';
+import Navbar1 from "../Components/Navbar/Navbar";
 
-import { AiOutlineArrowRight } from 'react-icons/ai';
+import { AiOutlineArrowRight } from "react-icons/ai";
 
 const Page = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 
-    gap: 3vw;
+  gap: 3vw;
 
-    background-color: #C8C8C8;
-`
+  background-color: #c8c8c8;
+`;
 
 const MainContents = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
 
-    gap: 2vw;
-`
+  gap: 2vw;
+`;
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    /* gap: 2vw; */
-`
+  display: flex;
+  flex-direction: column;
+  /* gap: 2vw; */
+`;
 
 const Desktop8 = () => {
   return (
     <Page>
-        <Navbar1 />
-        <MainContents>
-            <ShopDescription shopname="Lohit Canteen" shoptype="Canteen" phone="1234567890" adress="Lohit Hostel IITG" status="Open Now" distance="800m" checkpoint="Disang" noofreviews="14" services={["Food"]} />
-            <Container>
-                <div style={{backgroundColor: "white", textAlign: "left", width: "60vw", padding: "0.5vw 1.25vw 0.5vw 1.25vw"}} >
-                    <h1>Menu</h1>
-                </div>
-                <ItemCard itemname="Item Name" shopname="Lohit Canteen" cost="$ XXX" availability="Available" preptime="20mins"/>
-                <ItemCard itemname="Item Name" shopname="Lohit Canteen" cost="$ XXX" availability="Available" preptime="20mins"/>
-                <ItemCard itemname="Item Name" shopname="Lohit Canteen" cost="$ XXX" availability="Available" preptime="20mins"/>
-                <ItemCard itemname="Item Name" shopname="Lohit Canteen" cost="$ XXX" availability="Available" preptime="20mins"/>
-                <ItemCard itemname="Item Name" shopname="Lohit Canteen" cost="$ XXX" availability="Available" preptime="20mins"/>
-            </Container>
-        </MainContents>
+      <Navbar1 />
+      <MainContents>
+        <ShopDescription
+          shopname="Lohit Canteen"
+          shoptype="Canteen"
+          phone="1234567890"
+          adress="Lohit Hostel IITG"
+          status="Open Now"
+          distance="800m"
+          checkpoint="Disang"
+          noofreviews="14"
+          services={["Food"]}
+        />
+        <Container>
+          <div
+            style={{
+              backgroundColor: "white",
+              textAlign: "left",
+              width: "60vw",
+              padding: "0.5vw 1.25vw 0.5vw 1.25vw",
+            }}
+          >
+            <h1>Menu</h1>
+          </div>
+          <ItemCard
+            itemname="Item Name"
+            shopname="Lohit Canteen"
+            cost="$ XXX"
+            availability="Available"
+            preptime="20mins"
+          />
+          <ItemCard
+            itemname="Item Name"
+            shopname="Lohit Canteen"
+            cost="$ XXX"
+            availability="Available"
+            preptime="20mins"
+          />
+          <ItemCard
+            itemname="Item Name"
+            shopname="Lohit Canteen"
+            cost="$ XXX"
+            availability="Available"
+            preptime="20mins"
+          />
+          <ItemCard
+            itemname="Item Name"
+            shopname="Lohit Canteen"
+            cost="$ XXX"
+            availability="Available"
+            preptime="20mins"
+          />
+          <ItemCard
+            itemname="Item Name"
+            shopname="Lohit Canteen"
+            cost="$ XXX"
+            availability="Available"
+            preptime="20mins"
+          />
+        </Container>
+      </MainContents>
     </Page>
-  )
-}
+  );
+};
 
-export default Desktop8
+export default Desktop8;
--- user/src/Pages/Desktop9.jsx
+++ user/src/Pages/Desktop9.jsx
@@ -1,60 +1,128 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 import ShopDescription from "../Components/ShopDescription";
 import ItemCard from "../Components/ItemCard";
-import Navbar1 from '../Components/Navbar/Navbar';
+import Navbar1 from "../Components/Navbar/Navbar";
 
-import { AiOutlineArrowRight } from 'react-icons/ai';
+import { AiOutlineArrowRight } from "react-icons/ai";
 
 const Page = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 
-    gap: 3vw;
+  gap: 3vw;
 
-    background-color: #C8C8C8;
-`
+  background-color: #c8c8c8;
+`;
 
 const MainContents = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
 
-    gap: 2vw;
-`
+  gap: 2vw;
+`;
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    /* gap: 2vw; */
-`
+  display: flex;
+  flex-direction: column;
+  /* gap: 2vw; */
+`;
 
 const Desktop8 = () => {
   return (
     <Page>
-        <Navbar1 />
-        <MainContents>
-            <ShopDescription shopname="Core 1 Stationery" shoptype="Stationery Store" phone="1234567890" adress="Core-1 building near lecture hall, IITG" status="Open Now" distance="800m" checkpoint="Central Library" noofreviews="14" services={["Color Printing","Framing","Paper Cutting"]} />
-            <Container>
-                <a href="#" style={{textDecoration: "none", height: "4vw",display: "flex", flexDirection: "column", alignItems: "center",justifyContent: "center"}} >
-                    <div style={{backgroundColor: "#16153D", color:"white", textAlign: "left", width: "60vw", padding: "0vw 1.25vw"}} >
-                        <h1 style={{textDecoration: "none",display: "flex",alignItems: "center"}}>Send Items For Print <AiOutlineArrowRight/> </h1>
-                    </div>
-                </a>
-                <div style={{backgroundColor: "white", textAlign: "left", width: "60vw", padding: "0.5vw 1.25vw 0.5vw 1.25vw"}} >
-                    <h1>Catalogue</h1>
-                </div>
-                <ItemCard itemname="Item Name" shopname="Core 1 Stationery" cost="$ XXX" availability="Available" />
-                <ItemCard itemname="Item Name" shopname="Core 1 Stationery" cost="$ XXX" availability="Available" />
-                <ItemCard itemname="Item Name" shopname="Core 1 Stationery" cost="$ XXX" availability="Available" />
-                <ItemCard itemname="Item Name" shopname="Core 1 Stationery" cost="$ XXX" availability="Available" />
-                <ItemCard itemname="Item Name" shopname="Core 1 Stationery" cost="$ XXX" availability="Available" />
-            </Container>
-        </MainContents>
+      <Navbar1 />
+      <MainContents>
+        <ShopDescription
+          shopname="Core 1 Stationery"
+          shoptype="Stationery Store"
+          phone="1234567890"
+          adress="Core-1 building near lecture hall, IITG"
+          status="Open Now"
+          distance="800m"
+          checkpoint="Central Library"
+          noofreviews="14"
+          services={["Color Printing", "Framing", "Paper Cutting"]}
+        />
+        <Container>
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              height: "4vw",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <div
+              style={{
+                backgroundColor: "#16153D",
+                color: "white",
+                textAlign: "left",
+                width: "60vw",
+                padding: "0vw 1.25vw",
+              }}
+            >
+              <h1
+                style={{
+                  textDecoration: "none",
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                Send Items For Print <AiOutlineArrowRight />{" "}
+              </h1>
+            </div>
+          </a>
+          <div
+            style={{
+              backgroundColor: "white",
+              textAlign: "left",
+              width: "60vw",
+              padding: "0.5vw 1.25vw 0.5vw 1.25vw",
+            }}
+          >
+            <h1>Catalogue</h1>
+          </div>
+          <ItemCard
+            itemname="Item Name"
+            shopname="Core 1 Stationery"
+            cost="$ XXX"
+            availability="Available"
+          />
+          <ItemCard
+            itemname="Item Name"
+            shopname="Core 1 Stationery"
+            cost="$ XXX"
+            availability="Available"
+          />
+          <ItemCard
+            itemname="Item Name"
+            shopname="Core 1 Stationery"
+            cost="$ XXX"
+            availability="Available"
+          />
+          <ItemCard
+            itemname="Item Name"
+            shopname="Core 1 Stationery"
+            cost="$ XXX"
+            availability="Available"
+          />
+          <ItemCard
+            itemname="Item Name"
+            shopname="Core 1 Stationery"
+            cost="$ XXX"
+            availability="Available"
+          />
+        </Container>
+      </MainContents>
     </Page>
-  )
-}
+  );
+};
 
-export default Desktop8
+export default Desktop8;
