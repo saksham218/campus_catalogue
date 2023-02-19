347c165bf4a7e38390a20b3e6fd0784cab57caf1
Anurag Ravi <kumarayush1014@gmail.com>
2023-02-19 05:31:53 +0530

    modified deploy script

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -19,4 +19,27 @@ npm install --yes
 echo "Restarting pm2..."
 pm2 restart all
 
+# build shop frontend
+cd /home/anurag/campus_catalogue/shops
+yarn
+yarn build
+# move code from dist folder to server folder
+rm -r /var/www/shop.anuragr.live/*
+cp -r dist/* /var/www/shop.anuragr.live/
+
+# build user frontend
+cd /home/anurag/campus_catalogue/user
+yarn
+yarn build
+# move code from dist folder to server folder
+rm -r /var/www/customer.anuragr.live/*
+cp -r dist/* /var/www/customer.anuragr.live/
+
+# build admin frontend
+cd /home/anurag/campus_catalogue/admin_panel
+npm install
+npm run build
+# move code from dist folder to server folder
+rm -r /var/www/admin.anuragr.live/*
+cp -r build/* /var/www/admin.anuragr.live
 echo "Deployment Finished!"
