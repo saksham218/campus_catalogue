31c5d53f7cbdf0f66348fc1969fabeb7028d52fe
Anurag Ravi <kumarayush1014@gmail.com>
2023-02-25 12:41:17 +0530

    deploy script

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -19,6 +19,14 @@ npm install --yes
 echo "Restarting pm2..."
 pm2 restart all
 
+# build admin frontend
+cd /home/anurag/campus_catalogue/admin_panel
+npm install
+npm run build
+# move code from dist folder to server folder
+rm -r /var/www/admin.anuragr.live/*
+cp -r build/* /var/www/admin.anuragr.live
+
 # build shop frontend
 cd /home/anurag/campus_catalogue/shops
 yarn
@@ -35,11 +43,4 @@ yarn build
 rm -r /var/www/customer.anuragr.live/*
 cp -r dist/* /var/www/customer.anuragr.live/
 
-# build admin frontend
-cd /home/anurag/campus_catalogue/admin_panel
-npm install
-npm run build
-# move code from dist folder to server folder
-rm -r /var/www/admin.anuragr.live/*
-cp -r build/* /var/www/admin.anuragr.live
 echo "Deployment Finished!"
